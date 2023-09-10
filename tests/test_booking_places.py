679160db99002862ecea99a8f844ec11c8895db1
CharkaouiSalwa <salwacharkaoui@gmail.com>
2023-09-10 22:07:50 +0200

    update file test_booking_places.py

--- tests/test_booking_places.py
+++ tests/test_booking_places.py
@@ -13,7 +13,6 @@ club_name = "Simply Lift"
 # Test de la fonction book pour un cas où le booking réussit (compétition passée)
 def test_successful_booking_past():
     client = app.test_client()
-    # Assurez-vous que la compétition est passée (vous devez fournir la date passée)
     competition_date = datetime.strptime("2022-01-01 00:00:00", '%Y-%m-%d %H:%M:%S')
     response = client.get(f'/book/{competition_name}/{club_name}')
     assert response.status_code == 200
