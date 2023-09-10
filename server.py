01717581a86d007e31150b264f5bc721b18c1837
CharkaouiSalwa <salwacharkaoui@gmail.com>
2023-09-10 22:24:13 +0200

    update function purchasePlaces(), add file test_booking_places_12_places.py

--- server.py
+++ server.py
@@ -55,36 +55,42 @@ def book(competition,club):
 
 
 
-@app.route('/purchasePlaces',methods=['POST'])
+@app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
-    try:
-        competition_name = request.form['competition']
-        club_name = request.form['club']
-        places_required = int(request.form['places'])
+   try:
+       competition_name = request.form['competition']
+       club_name = request.form['club']
+       places_required = int(request.form['places'])
 
-        # Trouver la compétition et le club correspondants dans les listes
-        competition = next((c for c in competitions if c['name'] == competition_name), None)
-        club = next((c for c in clubs if c['name'] == club_name), None)
+       # Trouver la compétition et le club correspondants dans les listes
+       competition = next((c for c in competitions if c['name'] == competition_name), None)
+       club = next((c for c in clubs if c['name'] == club_name), None)
 
-        if competition is None or club is None:
-            flash("Club or competition not found.")
-        elif competition['numberOfPlaces'] < places_required:
-            flash("Not enough places available for booking.")
-        else:
-            # Vérifier si le club a suffisamment de points pour réserver
-            if club['points'] >= places_required:
-                # Déduire le nombre de places réservées de la compétition
-                competition['numberOfPlaces'] -= places_required
-                # Déduire les points du club
-                club['points'] -= places_required
-                flash(f"Booking complete! {places_required} places booked for {competition_name}")
-            else:
-                flash("Not enough points to make the booking.")
+       if competition is None or club is None:
+           flash("Club or competition not found.")
+       else:
+           # Convertir le nombre de points du club en entier
+           club_points = int(club['points'])
+           # Vérifier si le nombre de places demandées dépasse le maximum de 12
+           if places_required > 12:
+               flash("You can only book up to 12 places.")
+           else:
+               # Vérifier si le club a suffisamment de points pour réserver
+               if club_points >= places_required:
+                   # Déduire le nombre de places réservées de la compétition
+                   competition['numberOfPlaces'] -= places_required
+                   # Déduire les points du club
+                   club['points'] -= places_required
+                   flash('Booking complete! Number of places purchased: ' + str(places_required))
+               else:
+                   flash("Not enough points to make the booking.")
 
-        return render_template('welcome.html', club=club, competitions=competitions)
-    except Exception as e:
-        flash("An error occurred. Please try again.")
-        return redirect(url_for('index'))
+       return render_template('welcome.html', club=club, competitions=competitions)
+   except Exception as e:
+       flash("An error occurred. Please try again.")
+       return redirect(url_for('index'))
+
+
 
 
 
